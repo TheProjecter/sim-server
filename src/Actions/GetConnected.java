ed7c7d335ccb264cbe2ac7a17ff3e091bbad57b4
nico <nico@localhost>
2011-07-25 13:36:00 +0200

    for getting id connection

---
+++ src/Actions/GetConnected.java
@@ -0,0 +1,21 @@
+package Actions;
+
+import java.util.regex.Matcher;
+
+import Engine.Actions;
+import Engine.ListenerClient;
+
+public class GetConnected extends Actionnable{
+
+	public GetConnected(Actions pactions) {
+		super(pactions);
+
+	}
+
+
+	public void start(ListenerClient plc, Matcher m) {
+
+
+	}	
+
+}
