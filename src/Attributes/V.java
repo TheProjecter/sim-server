0f1ac5cad627e7481e47bc6fee2c4fe7f8ea1fd5
nico <nico@localhost>
2011-07-29 17:05:47 +0200

    attributes

---
+++ src/Attributes/V.java
@@ -0,0 +1,13 @@
+package Attributes;
+
+import Engine.ListenerClient;
+import Model.Attributable;
+
+public class V extends Attributable{
+
+	public String Get(String param,String p,ListenerClient plc){
+		
+		return "";
+	}
+	
+}
